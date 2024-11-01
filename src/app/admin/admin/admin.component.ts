7a2592d3e1915e1a133d89204c452bad192db167
Carlos Andres Pinilla Castrillon <106884751+charliepinilla777@users.noreply.github.com>
2024-10-31 21:20:45 -0500

    put workers in admin component and  social link

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService, Product } from '../admin/product.service';  // Asegúrate de que la ruta sea correcta
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  topSelling: Product[] = [];  // Define la propiedad topSelling en la clase
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.topSelling = this.productService.getProducts();  // Inicializa topSelling con los datos del servicio
+  }
 }
---
+++ src/app/admin/admin/product.service.ts
@@ -0,0 +1,61 @@
+// product.service.ts
+import { Injectable } from '@angular/core';
+
+export interface Product {
+    image: string;
+    uname: string;
+    gmail: string;
+    productName: string;
+    status: string;
+    weeks: number;
+    budget: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    // Define los productos aquí
+    TopSelling: Product[] = [
+        {
+            image: 'assets/images/tecnicos/mecanico5.jpg',
+            uname: 'Sergio Sandoval',
+            gmail: 'sandover@gmail.com',
+            productName: 'Tecnico en sincronizacion',
+            status: 'danger',
+            weeks: 1,
+            budget: 'ocupado'
+        },
+        {
+            image: 'assets/images/tecnicos/mecanico7.jpg',
+            uname: 'Daniel Arevalo',
+            gmail: 'daniel1234r@gmail.com',
+            productName: 'Mecanico general',
+            status: 'danger',
+            weeks: 2,
+            budget: 'ocupado'
+        },
+        {
+            image: 'assets/images/tecnicos/mecanico8.jpg',
+            uname: 'David Martinez',
+            gmail: 'martinez1254@gmail.com',
+            productName: 'Balanceo y alineacion',
+            status: 'danger',
+            weeks: 3,
+            budget: 'ocupado'
+        },
+        {
+            image: 'assets/images/tecnicos/mecanico9.jpg',
+            uname: 'Andres Perez',
+            gmail: 'perezandres@gmail.com',
+            productName: 'Electrico',
+            status: 'success',
+            weeks: 4,
+            budget: 'disponible'
+        },
+    ];
+
+    getProducts(): Product[] {
+        return this.TopSelling;
+    }
+}
