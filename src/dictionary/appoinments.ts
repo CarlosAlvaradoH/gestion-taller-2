6d50d40ecfd57f626a833f1a4e6eb5571c903482
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-11-08 22:09:29 -0500

    Add appoinment dicrionaries

---
+++ src/dictionary/appoinments.ts
@@ -0,0 +1,103 @@
+export const appoinmentsDay = [
+{
+    day: 20241108,
+    message: "Viernes 08 de Noviembre 2024"
+},
+{
+    day: 20241111,
+    message: "Lunes 11 de Noviembre 2024"
+},
+{
+    day: 20241112,
+    message: "Martes 12 de Noviembre 2024"
+},
+{
+    day: 20241113,
+    message: "Miercoles 13 de Noviembre 2024"
+},
+{
+    day: 20241114,
+    message: "Jueves 14 de Noviembre 2024"
+},
+{
+    day: 20241115,
+    message: "Viernes 15 de Noviembre 2024"
+},
+{
+    day: 20241118,
+    message: "Lunes 18 de Noviembre 2024"
+},
+{
+    day: 20241119,
+    message: "Martes 19 de Noviembre 2024"
+},
+{
+    day: 20241120,
+    message: "Miercoles 20 de Noviembre 2024"
+},
+{
+    day: 20241121,
+    message: "Jueves 21 de Noviembre 2024"
+},
+{
+    day: 20241122,
+    message: "Viernes 22 de Noviembre 2024"
+},
+{
+    day: 20241125,
+    message: "Lunes 25 de Noviembre 2024"
+},
+{
+    day: 20241126,
+    message: "Martes 26 de Noviembre 2024"
+},
+{
+    day: 20241127,
+    message: "Miercoles 27 de Noviembre 2024"
+},
+{
+    day: 20241128,
+    message: "Jueves 28 de Noviembre 2024"
+},
+{
+    day: 20241129,
+    message: "Viernes 29 de Noviembre 2024"
+},
+];
+
+
+
+
+export const appoinmentsHour = [
+{
+    turn: 1,
+    message: "08:00 am - 10:00 am"
+},
+{
+    turn: 2,
+    message: "10:00 am - 12:00 pm"
+},
+{
+    turn: 1,
+    message: "02:00 pm - 04:00 pm"
+},
+{
+    turn: 1,
+    message: "04:00 pm - 06:00 pm"
+},
+]
+
+export const appoinmentStatus = [
+    {
+        id: 1,
+        message: "Completado"
+    },
+    {
+        id: 2,
+        message: "En ejecucion"
+    },
+    {
+        id: 3,
+        message: "Fallido"
+    },
+]
