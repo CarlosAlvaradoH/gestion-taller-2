67f27568e487ed2bfa4f96d88120de6976df01d0
Carlos Andres Pinilla Castrillon <106884751+charliepinilla777@users.noreply.github.com>
2024-11-08 19:37:39 -0500

    Changes infroms, and size of the inputs

--- src/app/dashboard/dashboard-components/blog-cards/blog-cards-data.ts
+++ src/app/dashboard/dashboard-components/blog-cards/blog-cards-data.ts
@@ -11,25 +11,37 @@ export const blogcards: blogcard[] = [
         title: 'Sincronización Automotriz',
         subtitle: 'Servicio especializado',
         subtext: 'Incluye limpieza de inyectores y/o carburador, con mano de obra calificada para un rendimiento óptimo.',
-        image: 'assets/images/bg/sincronizacion.jpg'
+        image: '../../../../assets/images/bg/sincronizacion.jpg'
     },
     {
         title: 'Reparación y Mantenimiento',
         subtitle: 'Mano de obra calificada',
         subtext: 'Nuestros mecánicos cuentan con experiencia y están altamente calificados para brindar un servicio de calidad.',
-        image: 'assets/images/bg/mantenimiento.jpg'
+        image: '../../../../assets/images/bg/mantenimiento.jpg'
     },
     {
         title: 'Certificados de Gases',
         subtitle: 'Certificación ISO 9002-34',
         subtext: 'Contamos con todas las certificaciones legales necesarias para ofrecer la mejor calidad y experiencia a nuestros clientes.',
-        image: 'assets/images/bg/certificado.jpg'
+        image: '../../../../assets/images/bg/certificado.jpg'
     },
     {
         title: 'Alineación y Balanceo',
         subtitle: 'Vehículos y camionetas',
-        subtext: 'Utilizamos maquinaria de última tecnología para garantizar un servicio preciso y confiable.',
-        image: 'assets/images/bg/alineacion.jpg'
+        subtext: 'Utilizamos maquinaria de última tecnología para garantizar un servicio preciso y confiable..',
+        image: '../../../../assets/images/bg/alineacion.jpg'
+    },
+    {
+        title: 'Revisión de frenos y ABS',
+        subtitle: 'Cambio de pastillas y líquido de frenos',
+        subtext: 'Contamos con técnicos especializados y certificados en el mantenimiento de frenos y discos de frenos.',
+        image: '../../../../assets/images/bg/frenado.jpg'
+    },
+    {
+        title: 'Cambio de Aceite ',
+        subtitle: 'Contamos con las mejores marcas de aceites y consumibles para todo tipo de vehículo',
+        subtext: 'Productos garantizados .',
+        image: '../../../../assets/images/bg/aceite.jpg'
     }
 
 ] 
