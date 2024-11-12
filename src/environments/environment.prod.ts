2c51a0cb446c56c85f3484b635afda563efc2073
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-11-11 23:13:33 -0500

    Update environments urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,37 +2,32 @@
 const apiUrl = 'https://api-workshop-app.onrender.com/api/'
 
 export const environment = {
-    production: true,
-    postUserLogin: `${apiUrl}user/login`,
-    postUserRegister: `${apiUrl}user/register`,
-    getUserProfile: `${apiUrl}user/profile`,
-    putUserUpdate: `${apiUrl}user/updateUsers`,
-    patchUserDelete: `${apiUrl}user/delete`,
-    getUserList: `${apiUrl}user/listUsers`,
-    getUserListCustomer: `${apiUrl}user/listClients`,
-    patchUserActiveCustomer: `${apiUrl}user/activeClients`,
-    putUpdateProfile: `${apiUrl}user/updateProfile`,
+  production: false,
+  postUserLogin: `${apiUrl}user/login`,
+  postUserRegister: `${apiUrl}user/register`,
+  getUserProfile: `${apiUrl}user/profile`,
+  putUserUpdate: `${apiUrl}user/updateUsers`,
+  patchUserDelete: `${apiUrl}user/delete`,
+  getUserList: `${apiUrl}user/listUsers`,
+  getUserListCustomer: `${apiUrl}user/listClients`,
+  patchUserActiveCustomer: `${apiUrl}user/activeClients`,
+  putUpdateProfile: `${apiUrl}user/updateProfile`,
+  getlistMechanic: `${apiUrl}user/listMechanic`, // Se puede usar paginacion /1
+  
 
-    postSvCreate: `${apiUrl}service/create-service`,
-    getSv: `${apiUrl}service/services`,
-    getSvRename: `${apiUrl}service/service`,
-    putSvUpdate: `${apiUrl}service/upgrade-service`,
-    delSvDelete: `${apiUrl}service/delete`,
-    delSvHardDelete: `${apiUrl}service/permanent-erase`,
-    putSvReactivate: `${apiUrl}service/reactivate`,
-    
-    postApptCreate: `${apiUrl}service/create-appointment`,
-    getApptTest: `${apiUrl}`,
-    getApptGet: `${apiUrl}service/get-appointment`,
-    patchApptUpdate: `${apiUrl}service/update-appointment`,
-    delApptDelete: `${apiUrl}service/delete-appointment`,
-    
-    getTecTest: `${apiUrl}mechanic/test-mechanic`,
-    postTecCreate: `${apiUrl}mechanic/create-mechanic`,
-    getTecList: `${apiUrl}mechanic/list-mechanics`,
-    getTecGet: `${apiUrl}mechanic/get-mechanics`,
-    patchTecUpdate: `${apiUrl}mechanic/update-mechanic`,
-    patchTecDelete: `${apiUrl}mechanic/delete-mechanic`,
-    patchTecHardDelete: `${apiUrl}mechanic/permanent-erase`,
-    delTecLogin: `${apiUrl}mechanic/loginMechanic`,
-  };  
+  postSvCreate: `${apiUrl}service/create-service`,
+  getSv: `${apiUrl}service/services`,
+  getSvRename: `${apiUrl}service/service`,
+  putSvUpdate: `${apiUrl}service/upgrade-service`,
+  delSvDelete: `${apiUrl}service/delete`,
+  delSvHardDelete: `${apiUrl}service/permanent-erase`,
+  putSvReactivate: `${apiUrl}service/reactivate`,
+  
+  postApptCreate: `${apiUrl}appointment/create-appointment`,
+  getApptTest: `${apiUrl}`,
+  getApptGet: `${apiUrl}service/get-appointment`,
+  patchApptUpdate: `${apiUrl}service/update-appointment`,
+  delApptDelete: `${apiUrl}service/delete-appointment`,
+  getApptList: `${apiUrl}appointment/list-appointment`,
+  
+};  
